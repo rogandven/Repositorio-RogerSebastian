69e02c7297a06c6ace9938f8abd16c250864c3f5
Roger Andrés Venegas Opazo <roger.venegas2301@alumnos.ubiobio.cl>
2026-03-16 14:26:03 -0300

    added matrix creation

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+matrix = []
+array = None
+
 if __name__ == "__main__":
     print("hello world")
+    for i in range(0, 100):
+        array = []
+        for j in range(0, 100):
+            array.append(j)
+            print("[" + str(j) + "]", end="")
+        matrix.append(array)
+        print()
